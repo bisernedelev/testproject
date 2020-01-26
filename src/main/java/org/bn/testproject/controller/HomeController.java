209a8a1f7ceb2986c21f73edbf8ef4b7c0e9d6a7
Biser Nedelev <bibo0088@abv.bg>
2020-01-26 11:17:43 -0500

    Update HomeController.java

--- src/main/java/org/bn/testproject/controller/HomeController.java
+++ src/main/java/org/bn/testproject/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class HomeController {
     @GetMapping("/")
